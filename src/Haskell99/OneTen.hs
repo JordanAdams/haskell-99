763799d9e9a5ff63ae9fa54613afcec19dfa64f5
Jordan Adams <hi@jordanadams.co.uk>
2019-02-18 22:09:27 +0000

    completed 03 - 10

--- src/Haskell99/OneTen.hs
+++ src/Haskell99/OneTen.hs
@@ -1,11 +1,86 @@
 module Haskell99.OneTen where
 
+import Data.List (group, map)
+
 -- Problem 1:
 -- Find the last element of a list.
 last' :: [a] -> a
 last' = head . reverse
 
+
 -- Problem 2:
 -- Find the last but one element of a list.
 lastButOne :: [a] -> a
 lastButOne = head . drop 1 . reverse
+
+
+-- Problem 3:
+-- Find the K'th element of a list. The first element in the list is number 1.
+elementAt :: Int -> [a] -> a
+elementAt _ []    = error "out of bounds"
+elementAt 1 (x:_) = x
+elementAt n (x:xs)
+  | n > 1     = elementAt (n - 1) xs
+  | otherwise = error "out of bounds"
+
+
+-- Problem 4:
+-- Find the number of elements of a list.
+myLength :: [a] -> Int
+myLength xs = sum [1 | _ <- xs]
+
+-- Or with recursion:
+-- myLength [] = 0
+-- myLength (_:xs) = 1 + length xs
+
+
+-- Problem 5:
+-- Reverse a list.
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse (x:xs) = (myReverse xs) ++ [x]
+
+
+-- Problem 6:
+-- Find out whether a list is a palindrome.
+-- A palindrome can be read forward or backward; e.g. (x a m a x).
+isPalindrome :: (Eq a) => [a] -> Bool
+isPalindrome x = (reverse x) == x
+
+
+-- Problem 7:
+-- Flatten a nested list structure.
+data NestedList a = Elem a | List [NestedList a]
+flatten :: NestedList a -> [a]
+flatten (List []) = []
+flatten (List (x:xs)) = flatten x ++ flatten (List xs)
+flatten (Elem x) = [x]
+
+
+-- Problem 8:
+-- Eliminate consecutive duplicates of list elements.
+-- If a list contains repeated elements they should be replaced with a single copy of the element.
+-- The order of the elements should not be changed.
+compress :: (Eq a) => [a] -> [a]
+compress = (map head) . group
+
+-- Or without group:
+-- compress [] = []
+-- compress [x] = [x]
+-- compress (x:(y:ys))
+--   | x == y = compress (y : ys)
+--   | otherwise = x : compress (y : ys)
+
+-- Problem 9:
+-- Pack consecutive duplicates of list elements into sublists.
+-- If a list contains repeated elements they should be placed in separate sublists.
+pack :: [Char] -> [[Char]]
+pack "" = []
+pack (x:xs) = (x : takeWhile (== x) xs) : pack (dropWhile (== x) xs)
+
+-- Problem 10:
+-- Run-length encoding of a list.
+-- Use the result of problem P09 to implement the so-called run-length encoding data compression method.
+-- Consecutive duplicates of elements are encoded as lists (N E) where N is the number of duplicates of the element E.
+encode :: [Char] -> [(Char, Int)]
+encode = map (\x -> (head x, length x)) . pack
--- test/Haskell99/OneTenSpec.hs
+++ test/Haskell99/OneTenSpec.hs
@@ -1,18 +1,69 @@
 module Haskell99.OneTenSpec (spec) where
 
 import Test.Hspec
-import Haskell99.OneTen (last', lastButOne)
+import Control.Exception (evaluate)
+import Haskell99.OneTen
 
 spec :: Spec
 spec = do
   describe "01: last'" $ do
     it "should get the last item of a list" $ do
-      last' [1,2,3,4]       `shouldBe` (4 :: Int)
-      last' ['x', 'y', 'z'] `shouldBe` ('z' :: Char)
-      last' "xyz"           `shouldBe` ('z' :: Char)
+      last' [1,2,3,4]       `shouldBe` 4
+      last' ['x', 'y', 'z'] `shouldBe` 'z'
+      last' "xyz"           `shouldBe` 'z'
 
   describe "02: lastButOne" $ do
     it "should get the last but one element of a list" $ do
-      lastButOne [1,2,3,4]       `shouldBe` (3 :: Int)
-      lastButOne ['x', 'y', 'z'] `shouldBe` ('y' :: Char)
-      lastButOne "xyz"           `shouldBe` ('y' :: Char)
+      lastButOne [1,2,3,4]       `shouldBe` 3
+      lastButOne ['x', 'y', 'z'] `shouldBe` 'y'
+      lastButOne "xyz"           `shouldBe` 'y'
+
+  describe "03: elementAt" $ do
+    it "should get the element at the given index in a list" $ do
+      evaluate (elementAt 2 [])  `shouldThrow` errorCall "out of bounds"
+      evaluate (elementAt 2 [1]) `shouldThrow` errorCall "out of bounds"
+
+      elementAt 1 [1]     `shouldBe` 1 
+      elementAt 2 [1,2,3] `shouldBe` 2 
+      elementAt 2 "xyz"   `shouldBe` 'y'
+
+  describe "04: myLength" $ do
+    it "should get the length of the given list" $ do
+      myLength []          `shouldBe` 0
+      myLength [1,2,3,4,5] `shouldBe` 5
+      myLength "abc"       `shouldBe` 3
+
+  describe "05: myReverse" $ do
+    it "should reverse the given list" $ do
+      myReverse []          `shouldBe` ([] :: [Int])
+      myReverse [1,2,3,4,5] `shouldBe` [5,4,3,2,1]
+      myReverse "abc"       `shouldBe` "cba"
+
+  describe "06: isPalindrome" $ do
+    it "should check if the given list is a palindrome" $ do
+      isPalindrome ([] :: [Int]) `shouldBe` True
+      isPalindrome [1,2,2,1]     `shouldBe` True
+      isPalindrome "madamimadam" `shouldBe` True
+
+      isPalindrome [1,2,3]          `shouldBe` False
+      isPalindrome "notapalindrome" `shouldBe` False
+
+  describe "07: flatten" $ do
+    it "should flatten a nested list" $ do
+      flatten (List [(Elem 1), (List [(Elem 2), (Elem 3)])]) `shouldBe` [1,2,3]
+
+  describe "08: compress" $ do
+    it "should remove consecutive duplicates in the given list" $ do
+      compress []                    `shouldBe` ([] :: [Int])
+      compress [1,2,2,2,3,4,4,5]     `shouldBe` [1,2,3,4,5]
+      compress "aaabbccccccdddddcee" `shouldBe` "abcdce"
+
+  describe "09: pack" $ do
+    it "should combine consecutive duplicates into sublists" $ do
+      pack ""                    `shouldBe` []
+      pack "aaabbccccccdddddcee" `shouldBe` ["aaa", "bb", "cccccc", "ddddd", "c", "ee"]
+
+  describe "10: encode" $ do
+    it "should encode using run-length encoding" $ do
+      encode ""                    `shouldBe` []
+      encode "aaabbccccccdddddcee" `shouldBe` [('a', 3), ('b', 2), ('c', 6), ('d', 5), ('c', 1), ('e', 2)]
